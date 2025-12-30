698d584b7d3ebc8c70d55709ba1284314689e480
Wiwie Sanjaya <wiwiewei18@gmail.com>
2025-12-30 14:04:57 +0900

    feat(config): add config module

---
+++ src/config/config.ts
@@ -0,0 +1,13 @@
+export default () => ({
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  },
+  google: {
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  },
+  postgres: {
+    url: process.env.POSTGRES_URL,
+  },
+});
